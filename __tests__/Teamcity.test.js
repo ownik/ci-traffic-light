6669281d70e2dbb21556bf26ca168b0c95542997
ownik <ownik.dev@gmail.com>
2020-06-15 18:09:01 +0300

    Create Teacmity class for Teamcity api functions Fixes Teamcity tests Added test for Teamcity isFinishedBuildFail when no build availible

--- __tests__/Teamcity.test.js
+++ __tests__/Teamcity.test.js
@@ -1,39 +1,46 @@
 import mockAxios from 'axios';
-import { isFinishedBuildFail } from '../src/Teamcity';
+import { Teamcity } from '../src/Teamcity';
 
 jest.mock('axios', () => ({
   get: jest.fn().mockImplementation(() => Promise.resolve({ data: {} })),
 }));
 
-const makeJson = (buildTypeId, status) => ({
-  count: 1,
-  href: `/app/rest/builds?locator=branch:(default:true),failedToStart:any,running:false,canceled:false,count:1,buildType:(${buildTypeId})`,
-  nextHref: `/app/rest/builds?locator=branch:(default:true),failedToStart:any,running:false,canceled:false,count:1,buildType:${buildTypeId},start:1`,
-  build: [
-    {
-      id: 2,
-      buildTypeId: buildTypeId,
-      number: '2',
-      status: status,
-      state: 'finished',
-      href: '/app/rest/builds/id:2',
-      webUrl: `http://localhost:8111/viewLog.html?buildId=2&buildTypeId=${buildTypeId}`,
-    },
-  ],
-});
-const isFinishedBuildFailWrapper = () =>
-  isFinishedBuildFail('some url', 'some-branch', 'SomeBuildTypeId');
+const makeJson = (buildTypeId, statuses) => {
+  let buildArr = statuses.map((status, index) => ({
+    id: index,
+    buildTypeId: buildTypeId,
+    number: `${index}`,
+    status: status,
+    state: 'finished',
+    href: `/app/rest/builds/id:${index}`,
+    webUrl: `http://localhost:8111/viewLog.html?buildId=${index}&buildTypeId=${buildTypeId}`,
+  }));
+
+  return {
+    count: buildArr.length,
+    href: `/app/rest/builds?locator=branch:(default:true),failedToStart:any,running:false,canceled:false,count:1,buildType:(${buildTypeId})`,
+    nextHref: `/app/rest/builds?locator=branch:(default:true),failedToStart:any,running:false,canceled:false,count:1,buildType:${buildTypeId},start:1`,
+    build: buildArr,
+  };
+};
 
 describe('Teamcity', () => {
+  let teamcity;
   beforeEach(() => {
+    teamcity = new Teamcity('some url', 'some-branch');
+  });
+
+  afterEach(() => {
     mockAxios.get.mockClear();
   });
 
   test('isFinishedBuildFail test axios arguments', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'SUCCESS') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['SUCCESS']),
+      })
     );
-    await isFinishedBuildFail('some url', 'some-branch', 'SomeBuildTypeId');
+    await teamcity.isFinishedBuildFail('SomeBuildTypeId');
     expect(mockAxios.get).toHaveBeenCalledTimes(1);
     expect(
       mockAxios.get
@@ -43,66 +50,93 @@ describe('Teamcity', () => {
     );
   });
 
+  test('isFinishedBuildFail false when no builds', async () => {
+    mockAxios.get.mockImplementationOnce(() =>
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', []),
+      })
+    );
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(false);
+  });
+
   test('isFinishedBuildFail false when SUCCESS state upper case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'SUCCESS') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['SUCCESS']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(false);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(false);
   });
 
   test('isFinishedBuildFail false when success state lower case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'success') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['success']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(false);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(false);
   });
 
   test('isFinishedBuildFail false when suCCeSS state different case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'suCCeSS') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['suCCeSS']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(false);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(false);
   });
 
   test('isFinishedBuildFail false when UNKNOWN state upper case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'UNKNOWN') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['UNKNOWN']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(false);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(false);
   });
 
   test('isFinishedBuildFail false when unknown state lower case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'unknown') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['unknown']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(false);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(false);
   });
 
   test('isFinishedBuildFail false when unKnOWN state different case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'unKnOWN') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['unKnOWN']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(false);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(false);
   });
 
-  test('isFinishedBuildFail true when failure state upper case', async () => {
+  test('isFinishedBuildFail true when FAILURE state upper case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'FAILURE') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['FAILURE']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(true);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(true);
   });
 
   test('isFinishedBuildFail true when failure state lower case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'failure') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['failure']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(true);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(true);
   });
 
   test('isFinishedBuildFail true when faIlUrE state different case', async () => {
     mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: makeJson('SomeBuildTypeId', 'faIlUrE') })
+      Promise.resolve({
+        data: makeJson('SomeBuildTypeId', ['faIlUrE']),
+      })
     );
-    expect(await isFinishedBuildFailWrapper()).toEqual(true);
+    expect(await teamcity.isFinishedBuildFail('SomeBuildTypeId')).toEqual(true);
   });
 });
--- src/Teamcity.js
+++ src/Teamcity.js
@@ -6,15 +6,24 @@ export const STATUSES = {
   FAILURE: 'FAILURE',
 };
 
-export const isFinishedBuildFail = async (serverUrl, branch, buildTypeId) => {
-  const goodStatuses = [STATUSES.Success, STATUSES.Unknown];
-  const response = await axios.get(
-    `${serverUrl}/app/rest/builds?locator=branch:${branch},failedToStart:any,running:false,canceled:false,count:1,buildType:(${buildTypeId})`,
-    {
-      headers: {
-        Accept: 'application/json',
-      },
-    }
-  );
-  return !goodStatuses.includes(response.data.build[0].status.toUpperCase());
-};
+export class Teamcity {
+  constructor(serverUrl, branch) {
+    this.serverUrl = serverUrl;
+    this.branch = branch;
+  }
+
+  async isFinishedBuildFail(buildTypeId) {
+    const goodStatuses = [STATUSES.Success, STATUSES.Unknown];
+    const response = await axios.get(
+      `${this.serverUrl}/app/rest/builds?locator=branch:${this.branch},failedToStart:any,running:false,canceled:false,count:1,buildType:(${buildTypeId})`,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      }
+    );
+    return response.data.build.length > 0
+      ? !goodStatuses.includes(response.data.build[0].status.toUpperCase())
+      : false;
+  }
+}
