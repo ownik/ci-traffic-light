f7a10f15f8744f47848cb9045206baa82b76133c
ownik <ownik.dev@gmail.com>
2020-09-15 22:31:31 +0300

    Update to webpack 5 Updated other modules Removing unused modules

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CreateFileWebpack = require('create-file-webpack');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -11,17 +10,4 @@ module.exports = merge(common, {
     writeToDisk: true,
     port: 8080,
   },
-  plugins: [
-    new CreateFileWebpack({
-      path: './dist',
-      fileName: 'settings.json',
-      content: `{
-        "serverUrl" : "http://localhost:8111",
-        "auth" : {"username" : "root", "password" : "123456"},
-        "branch" : "(default:true)",
-        "buildTypes" : ["CiIndicatorTest_Build1", "CiIndicatorTest_Build2"],
-        "updateStateInterval" : 10000
-      }`,
-    }),
-  ],
 });
