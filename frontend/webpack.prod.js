d9cd346e65676fd335dfea180e80449ecec580e6
Nikita Ostrenkov <xfakehopex@gmail.com>
2021-12-01 15:54:27 +0300

    Added TerserPlugin plugin to the production build

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,11 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   output: {
     path: path.join(__dirname, '../public'),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 });
