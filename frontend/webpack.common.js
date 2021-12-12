0df64ec510493d80588b76dc4482fcfd6834cc04
Nikita Ostrenkov <xfakehopex@gmail.com>
2021-12-12 20:52:26 +0300

    Updated page title

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Production',
+      title: 'CI Traffic Light',
       template: './src/index.template.html',
     }),
     new EnvironmentPlugin({
